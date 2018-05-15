29b17fbcb6be57c16af2ae43096274a7b93ed1eb
Jonathan Dust <d24dust@hotmail.com>
2018-05-15 16:45:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
   # write code here
+  attr_accessor :sides
+  def initialize(length1, length2, length3)
+    @sides << [length1, length2, length3]
+    @sides.flatten!
+  end
+
+  def kind
+    case @sides.uniq.length
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+  end
+
+  end
+
+  class TriangleError < StandardError
+  end
 end
